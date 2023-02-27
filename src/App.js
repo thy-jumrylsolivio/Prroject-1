3aea53a2850c930408c0ae3d7dee5c076051cf56
Jumryl <thy.jumrylsolivio@gmail.com>
2023-02-27 12:37:50 +0800

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import Counter from "./Meals/Counter";
 import MealsList from "./Meals/MealsList";
 import MealsProvider from "./Meals/MealsProvider";
+import AppWallet from "./walllet/AppWallet";
 
 function App() {
    
-    return(
-        <div>
-            <MealsProvider>
-                <MealsList/>
-                <Counter/>
-            </MealsProvider>
-        </div>
-    )
+    return <AppWallet/>
 }
 
 export default App;
